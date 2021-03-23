de9a75df0e5df4bb8476277e9c984518b335b527
Danny Grunbaum <random@uw.edu>
2021-03-22 20:25:10 -0700

    initial commit, draft module for ADS1115 voltage sensor

---
+++ Sensors/Voltage/read_volt.py
@@ -0,0 +1,97 @@
+# This script prints temperature readings from a DS18B20 sensor
+
+# Use driver by roberthh from https://github.com/robert-hh/ads1x15
+from Voltage.ads1x15 import ADS1115
+#from Light.tsl25x1 import tsl25x1_sensor #, TSL2561, Tsl2591, read_tsl25x1
+from time import sleep_ms
+from os import sync
+
+#global full, ir, lux
+global raw0, raw1, raw2, raw3
+global volt0, volt1, volt2, volt3
+# -------------------------------------------------------------------------------
+# Set up pins for the light sensors; power from either Vbat or p_pwr1 pin (defined in platform_defs)
+# -------------------------------------------------------------------------------
+class read_volt:
+
+    def __init__(self,lcd=False,i2c=None,rtc=None,addr=72,gain=4,rate=0):
+        self.i2c=i2c
+        self.lcd=lcd
+        self.rtc=rtc
+        self.logging=False
+        self.logfilename=None
+        self.logfile=None
+        self.log_format=None
+        self.fmt_keys=None
+        self.sample_num=0
+
+        self.addr=addr
+        self.gain=gain   # previous value gain=2
+        self.rate=rate   # previous value rate=4
+
+
+        # Wrapper function to call ADS1115 I2C voltage sensor
+        self.sensor = ADS1115(i2c, self.addr, self.gain)
+        #self.sensor = tsl25x1_sensor(i2c=self.i2c)
+
+    # -------------------------------------------------------------------------------
+    # Test the light sensor
+    # -------------------------------------------------------------------------------
+    def test_volt(self):
+        global raw0, raw1, raw2, raw3
+        global volt0, volt1, volt2, volt3
+
+        try: # Try to take a measurement, return 1 if successful, 0 if not
+            raw0=self.sensor.read(self.rate,0)
+            volt0=self.sensor.raw_to_v(raw0)
+            print('test: volt0 = ',volt0)
+            return 1
+        except:
+            return 0
+        
+    # -------------------------------------------------------------------------------
+    # Progression for obtaining voltage 0-3 readings from the sensor
+    # -------------------------------------------------------------------------------
+
+    def print_volt(self):
+        global raw0, raw1, raw2, raw3
+        global volt0, volt1, volt2, volt3
+
+        raw0=self.sensor.read(self.rate,0)
+        volt0=self.sensor.raw_to_v(raw0)
+        raw1=self.sensor.read(self.rate,0)
+        volt1=self.sensor.raw_to_v(raw0)
+        raw2=self.sensor.read(self.rate,0)
+        volt2=self.sensor.raw_to_v(raw0)
+        raw3=self.sensor.read(self.rate,0)
+        volt3=self.sensor.raw_to_v(raw0)
+
+        print('volt0: ',str(volt0),'volt1: ',str(volt1),'volt2: ',str(volt2),'volt3: ',str(volt3))
+        if self.lcd is not False:
+            try:
+                self.lcd.clear()      # Sleep for 1 sec
+                #self.lcd.putstr(str(round(lux,1))+' lux\n('+str(full)+','+str(ir)+')')
+                self.lcd.putstr('volts: '+str(round(volt0,3))+','+str(round(volt1,3))+',\n'+str(round(volt2,3))+','+str(round(volt3,3)))
+            except:
+                pass
+        if self.logging:
+            timestamp=tuple([list(self.rtc.datetime())[d] for d in [0,1,2,4,5,6]])
+            self.sample_num+=1
+            print(self.fmt_keys)
+            for s in self.fmt_keys:
+                print(s)
+                print(eval(s))
+            data=[self.sample_num]
+            data.extend([t for t in timestamp])
+            data.extend([eval(s) for s in self.fmt_keys])
+            print('data = ',data)
+            print('self.log_format=',self.log_format)
+            log_line=self.log_format % tuple(data)
+            print('log_line = ',log_line)
+            print('logging to filename: ',self.logfilename)
+            logfile=open(self.logfilename,'a')
+            logfile.write(log_line)
+            logfile.close()
+            sync()
+            sleep_ms(250)
+            
